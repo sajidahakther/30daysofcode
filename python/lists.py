da025e99f746cffb773e3d4b9bb8b623157df29f
Sajidah Akther <32687682+sajidahakther@users.noreply.github.com>
2018-06-06 19:01:47 +0100

    completed lists

--- python/lists.py
+++ python/lists.py
@@ -89,3 +89,61 @@ residents = {'Puffin' : 104, 'Sloth' : 105, 'Burmese Python' : 106}
 print residents['Puffin'] 
 print residents['Sloth']
 print residents['Burmese Python']
+
+#011 ADDING KEY-VALUE PAIRS
+menu = {} 
+menu['Chicken Alfredo'] = 14.50 
+print menu['Chicken Alfredo']
+
+# Dish-price pairs to menu
+menu['Pasta'] = 12.60
+menu['Pizza'] = 17.00
+menu['Lasagna'] = 13.20
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
+
+#012 DELETING ENTRIES AND OVERRIDDING VALUES
+zoo_animals = { 'Unicorn' : 'Cotton Candy House',
+'Sloth' : 'Rainforest Exhibit',
+'Bengal Tiger' : 'Jungle House',
+'Atlantic Puffin' : 'Arctic Exhibit',
+'Rockhopper Penguin' : 'Arctic Exhibit'}
+
+del zoo_animals['Unicorn']
+del zoo_animals['Sloth']
+del zoo_animals['Bengal Tiger']
+
+zoo_animals['Rockhopper Penguin'] = 'Rainforest Exhibit'
+
+print zoo_animals
+
+#013 REMOVING ITEMS
+backpack = ['xylophone', 'dagger', 'tent', 'bread loaf']
+backpack.remove('dagger')
+
+#014 ADDING/SORTING/REMOVING - Key-value pairs
+inventory = {
+  'gold' : 500,
+  # Assigning a list to 'pouch' and 'backpack' key
+  'pouch' : ['flint', 'twine', 'gemstone'], 
+  'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort() 
+
+# Adding a key 'pocket' and assigning a list to it
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+
+# Sorting the list found under the key 'backpack'
+inventory['backpack'].sort()
+
+# Removing 'dagger' from backpack
+inventory['backpack'].remove('dagger')
+
+# Adding 50 to the number stored under the 'gold' key
+inventory['gold'] = 550
