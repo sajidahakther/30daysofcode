39fe0ab298aeca8dc970645f5ccad2ca4f5bd3bc
SA <32687682+sajidahakther@users.noreply.github.com>
2018-06-13 22:50:53 +0100

    exercises #1-3

---
+++ python/loops.py
@@ -0,0 +1,19 @@
+#001 - prints "Hello, I am an if statement and count is 0" once, 
+# then "Hello, I am a while and count is 9" 9 times
+count = 0
+
+if count < 10:
+  print "Hello, I am an if statement and count is", count
+
+while count < 10:
+  print "Hello, I am a while and count is", count
+  count += 1
+  
+#002 - prints "i am a loop" once
+loop_condition = True
+
+while loop_condition:
+  print "I am a loop"
+  loop_condition = False
+  
+#003
