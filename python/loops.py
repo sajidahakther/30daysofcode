362b16ccbad14a1b07ae6429fc12a52091c14ff2
SA <32687682+sajidahakther@users.noreply.github.com>
2018-06-18 09:39:52 +0100

    #8 number guessing game

--- python/loops.py
+++ python/loops.py
@@ -62,3 +62,21 @@ while count < 3:
   count += 1
 else:
   print "You win!"
+  
+#008 Guess the number game
+from random import randint
+
+# Generates a number from 1 through 10 inclusive
+random_number = randint(1, 10)
+
+guesses_left = 3
+
+while guesses_left != 0:
+  guess = int(raw_input("Your guess: "))
+  if (guess == random_number):
+    print "You win!"
+    break
+  guesses_left = guesses_left - 1
+else: 
+  print "You lose."
+
