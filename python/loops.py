fed73f652d02ee6367b99bd1a360aeef52cb9853
SA <32687682+sajidahakther@users.noreply.github.com>
2018-06-23 21:22:05 +0100

    #13

--- python/loops.py
+++ python/loops.py
@@ -114,3 +114,14 @@ for char in phrase:
     print char,
     
 print
+
+#013 - print number squared
+numbers  = [7, 9, 12, 54, 99]
+
+print "This list contains: "
+
+for num in numbers:
+  print num
+
+for num in numbers:
+  print num ** 2
